5d39a43f11e965d486d1114453d33e873535ef2c
iron <aaa@aaa.com>
2025-08-16 01:43:35 -0400

    blocking messages from improperly categorized by npc dialogue

--- src/client/java/dev/wiji/wynntracker/mixin/client/MatchPatternMixin.java
+++ src/client/java/dev/wiji/wynntracker/mixin/client/MatchPatternMixin.java
@@ -5,6 +5,7 @@ import com.wynntils.handlers.chat.type.MessageType;
 import com.wynntils.handlers.chat.type.RecipientType;
 import dev.wiji.wynntracker.controllers.DiscordBridge;
 import dev.wiji.wynntracker.controllers.SocketMessageHandler;
+import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,6 +22,11 @@ public class MatchPatternMixin {
     )
     private void onMatchPattern(StyledText msg, MessageType messageType, CallbackInfoReturnable<Boolean> cir) {
         if ((Object)this == RecipientType.GUILD && messageType == MessageType.FOREGROUND) {
+            if (!filterNpcChatReset(msg)) {
+                cir.setReturnValue(false);
+                return;
+            }
+
             String text = null;
             try {
                 text = (String) StyledText.class.getMethod("getString").invoke(msg);
@@ -29,6 +35,31 @@ public class MatchPatternMixin {
             if (text == null) text = msg.toString();
 
             if (text.contains(DiscordBridge.GUILD_CHAT_PREFIX_FLAG) || text.contains(DiscordBridge.DISCORD_MESSAGE_SEQUENCE)) cir.setReturnValue(true);
+        }
+    }
+
+    private boolean filterNpcChatReset(StyledText styledText) {
+        try {
+            var firstPart = styledText.getFirstPart();
+
+            if (firstPart == null) {
+                return false;
+            }
+
+            var partStyle = firstPart.getPartStyle();
+            if (partStyle == null) {
+                return false;
+            }
+
+            var style = partStyle.getStyle();
+            if (style == null || style.getColor() == null) {
+                return false;
+            }
+
+            return style.getColor().equals(Formatting.DARK_GRAY.getColorValue());
+
+        } catch (Exception e) {
+            return true;
         }
     }
 }
