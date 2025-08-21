5229c9479940508d08c42d8dc6abb813426131fe
wiji <wijigaming.yt@gmail.com>
2025-08-21 16:30:27 -0700

    optional mod-menu integration

--- src/client/java/dev/wiji/tbgm/controllers/Config.java
+++ src/client/java/dev/wiji/tbgm/controllers/Config.java
@@ -1,11 +1,6 @@
 package dev.wiji.tbgm.controllers;
 
 import com.google.gson.Gson;
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.clothconfig2.api.ConfigBuilder;
-import me.shedaniel.clothconfig2.api.ConfigCategory;
-import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
@@ -17,7 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Config implements ModMenuApi {
+public class Config {
 	private static final Path configDir = Paths.get(MinecraftClient.getInstance().runDirectory.getPath() + "/config");
 	private static final Path configFile = Paths.get(configDir + "/gasmask.json");
 	private static ConfigData configData;
@@ -50,19 +45,8 @@ public class Config implements ModMenuApi {
 	}
 
 	public static Screen createConfigScreen(Screen parent) {
-		ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent).setTitle(Text.literal("Gasmask Config"));
-
-		ConfigCategory general = builder.getOrCreateCategory(Text.literal("General Config"));
-		ConfigEntryBuilder entryBuilder = builder.entryBuilder();
-
-		builder.setSavingRunnable(configData::save);
-
-		return builder.build();
-	}
-
-	@Override
-	public ConfigScreenFactory<?> getModConfigScreenFactory() {
-		return Config::createConfigScreen;
+		// Simple fallback screen when cloth-config is not available
+		return parent;
 	}
 
 	public static class ConfigData {
