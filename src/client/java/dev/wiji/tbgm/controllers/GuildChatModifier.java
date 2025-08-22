2a5439bc42c831be2c347f326c275c6f5904a60d
iron <aaa@aaa.com>
2025-08-21 22:30:48 -0400

    Revert "Merge branch 'master' of https://github.com/TheBrokenGasmask/GasmaskClient"
    This reverts commit 5d52dbe6eb0e5816b778440bb5f6fd72c96f8b1d, reversing
    changes made to 30dc45fdf995c6c768b4a7333de969629aee9fea.

--- src/client/java/dev/wiji/tbgm/controllers/GuildChatModifier.java
+++ src/client/java/dev/wiji/tbgm/controllers/GuildChatModifier.java
@@ -32,9 +32,7 @@ public class GuildChatModifier {
         if (siblingCount == 3) {
             Text prefixSibling = originalMessage.getSiblings().getFirst();
             boolean isFlagPole = prefixSibling.getString().contains(DiscordBridge.GUILD_CHAT_PREFIX_FLAGPOLE);
-
-            if (prefixSibling.getStyle().getColor() == null) return originalMessage;
-            boolean isAqua = prefixSibling.getStyle().getColor().getRgb() == Formatting.AQUA.getColorValue();
+            boolean isAqua = Objects.requireNonNull(prefixSibling.getStyle().getColor()).getRgb() == Formatting.AQUA.getColorValue();
 
             System.out.println("GuildChatModifier: isFlagPole = " + isFlagPole);
             System.out.println("GuildChatModifier: isAqua = " + isAqua);
